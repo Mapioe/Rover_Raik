ac27765da055221fc1388d801af94a1fa7107048
Mapioe <96563902+Mapioe@users.noreply.github.com>
2025-02-13 23:58:59 +0500

    Код нерабочий, обновленный
    Приколы с объявлением переменных в if и не только

--- Rover_Raik.ino
+++ Rover_Raik.ino
@@ -17,68 +17,74 @@ void setup() {
   pinMode(ForwardPinLeft, OUTPUT);
   pinMode(BackwardPinRight, OUTPUT);
   pinMode(JoyStickButton, INPUT_PULLUP);
+  JoyStickButtonStage = digitalRead(JoyStickButton);
 }
 
 void loop() {
   {
-  JoyStickButtonStage = digitalRead(JoyStickButton);
-    if (JoyStickButtonStage == LOW)
+    if (JoyStickButtonStage == LOW){
       ButtonCount++;
-      delay()
-    if (ButtonCount == 3) //Если кнопка нажата 3 раза включает режим дебага(Запоминания маршрута)
+      delay(500);
+    }
+    if (ButtonCount == 3){ //Если кнопка нажата 3 раза включает режим дебага (Запоминания маршрута)
       DebugMod = true;
-    else if (ButtonCount == 6)
+    }
+    else if (ButtonCount == 6){
       ButtonCount = 0;
       DebugMod = false;
+    }
   }
   {
 int HorizontalJoyStick = analogRead(HorizontalPin); 
 int VerticalJoyStick = analogRead(VerticalPin);
-if (HorizontalJoyStick > 450 and HorizontalJoyStick < 550) // <- Код с хабра для вычисления позиции джойстика.
-  HorizontalJoyStickPos = 0;                               // Производить запись, если дебаг режим. 
-if (HorizontalJoyStick >= 550)
-  HorizontalJoyStickPos = map(HorizontalJoyStick, 550, 1023, 0, 5);
-if (HorizontalJoyStick <= 450)
-  HorizontalJoyStickPos = map(HorizontalJoyStick, 450, 0, 0, -5); 
-if (VerticalJoyStick > 450 and VerticalJoyStick < 550) 
+if (HorizontalJoyStick > 450 and HorizontalJoyStick < 550){ // <- Код с хабра для вычисления позиции джойстика.
+  HorizontalJoyStickPos = 0;                               // Производить запись, если дебаг режим.
+} 
+if (HorizontalJoyStick >= 550){
+  HorizontalJoyStickPos = map(HorizontalJoyStick, 550, 1023, 0, 5); // Последнее число 255?
+}
+if (HorizontalJoyStick <= 450){
+  HorizontalJoyStickPos = map(HorizontalJoyStick, 450, 0, 0, -5);
+}
+if (VerticalJoyStick > 450 and VerticalJoyStick < 550){ 
   VerticalJoyStickPos = 0;
-if (VerticalJoyStick >= 550)
+}
+if (VerticalJoyStick >= 550){
   VerticalJoyStickPos = map(VerticalJoyStick, 550, 1023, 0, 5);
-if (VerticalJoyStick <= 450)
+}
+if (VerticalJoyStick <= 450){
   VerticalJoyStickPos = map(VerticalJoyStick, 450, 0, 0, -5);
-  }
+}
+ }
   
-  int HorizontalPosToMotor = (HorizontalJoyStickPos / 4); 
-  int VerticalPosToMotor = (VerticalJoyStickPos / 4);
-  
-  if (VerticalJoyStickPos > 512){
+  int abs(HorizontalPosToMotor) = ((HorizontalJoyStickToPos - 512) / 2); 
+  int abs(VerticalPosToMotor) = ((VerticalJoyStickToPos - 512) / 2);
+  int Speed = (((HorizontalPosToMotor / 255) * Rotation) * Dw); //Dw = Диаметр колеса. Rotation = Максимальная скорость вращения двигателя.
+
+  do {
+    if (VerticalJoyStickPos > 512){
   analogWrite(ForwardPinLeft, VerticalJoyStickPos);
   analogWrite(ForwardPinRight, VerticalJoyStickPos);
   }
-else{
-  int abs(VerticalPosToMotor) = ((VerticalJoyStickToPos - 512) / 2);
+    else{
   analogWrite(BackwardPinLeft, VerticalPosToMotor); 
   analogWrite(BackwardPinRight, VerticalPosToMotor);
   }
-if (HorizontalJoyStickPos > 512){
+    if (HorizontalJoyStickPos > 512){
   analogWrite(ForwardPinLeft,HorizontalPosToMotor);
-  Speed = ((HorizontalPosToMotor / 255) * 4.666) * D //D = Диаметр колеса
   delay(Speed / (0.25 * lwheel) * 1000); // lwheel = Расстояния между колесами. Функция поворота на 90 градусов.
   analogWrite(ForwardPinLeft, HorizontalPosToMotor);
   analogWrite(ForwardPinRight, HorizontalPosToMotor);
   }
-else{
-  int abs(HorizontalPosToMotor) = ((HorizontalJoyStickToPos - 512) / 2);
-  analogWrite(ForwardPinRight,HorizontalPosToMotor)
-  Speed = ((HorizontalPosToMotor / 255) * 4.666) * D
+    else{
+  analogWrite(ForwardPinRight,HorizontalPosToMotor);
   delay(Speed / (0.25 * lwheel) * 1000); 
   analogWrite(ForwardPinLeft, HorizontalPosToMotor);
   analogWrite(ForwardPinRight,HorizontalPosToMotor);
   }
-  if DebugMod == True{
-    int SomeKindOfArray [] = (SomeMotorValue)
-    int SomeKindOfArray [] = (SomeMotorValue)
-    int SomeKindOfArray [] = (SomeMotorValue)
-    int SomeKindOfArray [] = (SomeMotorValue)
-    }
+  int ForwardPinLeftArray [] = (VerticalPosToMotor);
+  int BackwardPinLeftArray [] = (VerticalPosToMotor);
+  int ForwardPinRightArray [] = (SomeMotorValue);
+  int BackwardPinRightArray [] = (SomeMotorValue);
+    } while (DebugMod == True);
 }
