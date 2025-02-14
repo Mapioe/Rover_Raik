8f7e0d0f2a66e474fd00216f4b78c922ab384741
Mapioe <96563902+Mapioe@users.noreply.github.com>
2025-02-14 17:46:29 +0500

    Программа компилится

--- Rover_Raik.ino
+++ Rover_Raik.ino
@@ -3,13 +3,15 @@ const int BackwardPinLeft = 5;  // Контакт для управления д
 const int ForwardPinRight = 10;
 const int BackwardPinRight = 9;
 
-const int HorizontalPin = A0;  //Контакт для приёма сигналов от джойстика 
-const int VerticalPin = A1;
+const int HorizontalPin = A1;  //Контакт для приёма сигналов от джойстика 
+const int VerticalPin = A2;
 
-const int JoyStickButton = 2;
+const int JoyStickButton = A3;
 bool DebugMod = false;  // Специальный дебаг режим для запоминания движения
-int ButtonCount = 0;
 
+float Rotation = 4.6666; //Rotation = Максимальная скорость вращения двигателя.
+int Lwheel = 150; // Lwheel = Расстояния между колесами. Функция поворота на 90 градусов. В мм?
+int Dwheel = 50; //Dwheel = Диаметр колеса.
 
 void setup() {
   pinMode(ForwardPinLeft, OUTPUT);
@@ -17,15 +19,15 @@ void setup() {
   pinMode(ForwardPinLeft, OUTPUT);
   pinMode(BackwardPinRight, OUTPUT);
   pinMode(JoyStickButton, INPUT_PULLUP);
-  JoyStickButtonStage = digitalRead(JoyStickButton);
 }
 
 void loop() {
   {
-    if (JoyStickButtonStage == LOW){
+    int ButtonCount = 0;
+     if (digitalRead(JoyStickButton) == LOW ){
       ButtonCount++;
       delay(500);
-    }
+    } 
     if (ButtonCount == 3){ //Если кнопка нажата 3 раза включает режим дебага (Запоминания маршрута)
       DebugMod = true;
     }
@@ -34,11 +36,13 @@ void loop() {
       DebugMod = false;
     }
   }
-  {
+do{
 int HorizontalJoyStick = analogRead(HorizontalPin); 
 int VerticalJoyStick = analogRead(VerticalPin);
+int HorizontalJoyStickPos = 0;
+int VerticalJoyStickPos = 0;
 if (HorizontalJoyStick > 450 and HorizontalJoyStick < 550){ // <- Код с хабра для вычисления позиции джойстика.
-  HorizontalJoyStickPos = 0;                               // Производить запись, если дебаг режим.
+  HorizontalJoyStickPos = 0;
 } 
 if (HorizontalJoyStick >= 550){
   HorizontalJoyStickPos = map(HorizontalJoyStick, 550, 1023, 0, 5); // Последнее число 255?
@@ -54,37 +58,40 @@ if (VerticalJoyStick >= 550){
 }
 if (VerticalJoyStick <= 450){
   VerticalJoyStickPos = map(VerticalJoyStick, 450, 0, 0, -5);
-}
- }
   
-  int abs(HorizontalPosToMotor) = ((HorizontalJoyStickToPos - 512) / 2); 
-  int abs(VerticalPosToMotor) = ((VerticalJoyStickToPos - 512) / 2);
-  int Speed = (((HorizontalPosToMotor / 255) * Rotation) * Dw); //Dw = Диаметр колеса. Rotation = Максимальная скорость вращения двигателя.
-
-  do {
-    if (VerticalJoyStickPos > 512){
-  analogWrite(ForwardPinLeft, VerticalJoyStickPos);
-  analogWrite(ForwardPinRight, VerticalJoyStickPos);
-  }
-    else{
-  analogWrite(BackwardPinLeft, VerticalPosToMotor); 
-  analogWrite(BackwardPinRight, VerticalPosToMotor);
-  }
-    if (HorizontalJoyStickPos > 512){
-  analogWrite(ForwardPinLeft,HorizontalPosToMotor);
-  delay(Speed / (0.25 * lwheel) * 1000); // lwheel = Расстояния между колесами. Функция поворота на 90 градусов.
-  analogWrite(ForwardPinLeft, HorizontalPosToMotor);
-  analogWrite(ForwardPinRight, HorizontalPosToMotor);
-  }
-    else{
-  analogWrite(ForwardPinRight,HorizontalPosToMotor);
-  delay(Speed / (0.25 * lwheel) * 1000); 
-  analogWrite(ForwardPinLeft, HorizontalPosToMotor);
-  analogWrite(ForwardPinRight,HorizontalPosToMotor);
-  }
-  int ForwardPinLeftArray [] = (VerticalPosToMotor);
-  int BackwardPinLeftArray [] = (VerticalPosToMotor);
-  int ForwardPinRightArray [] = (SomeMotorValue);
-  int BackwardPinRightArray [] = (SomeMotorValue);
-    } while (DebugMod == True);
+}
+int A = ((HorizontalJoyStickPos - 512) / 2) - 1; //Используем костыль вместо велосипеда.
+int HorizontalPosToMotor = abs(A);
+int B = ((VerticalJoyStickPos - 512) / 2) - 1;
+int VerticalPosToMotor = abs(B);
+int Speed = (((HorizontalPosToMotor / 255) * Rotation) * Dwheel);
+  if (VerticalJoyStickPos > 512){
+analogWrite(ForwardPinLeft, VerticalPosToMotor);
+analogWrite(ForwardPinRight, VerticalPosToMotor);
+}
+  else{
+analogWrite(BackwardPinLeft, VerticalPosToMotor); 
+analogWrite(BackwardPinRight, VerticalPosToMotor);
+}
+  if (HorizontalJoyStickPos > 512){
+analogWrite(ForwardPinLeft,HorizontalPosToMotor);
+delay(Speed / (0.25 * Lwheel) * 1000); 
+analogWrite(ForwardPinLeft, HorizontalPosToMotor);
+analogWrite(ForwardPinRight, HorizontalPosToMotor);
+}
+  else{
+analogWrite(ForwardPinRight,HorizontalPosToMotor);
+delay(Speed / (0.25 * Lwheel) * 1000); 
+analogWrite(ForwardPinLeft, HorizontalPosToMotor);
+analogWrite(ForwardPinRight,HorizontalPosToMotor);
+}
+int * ForwardPinLeftArray;
+ForwardPinLeftArray = (VerticalPosToMotor);
+int * BackwardPinLeftArray;
+BackwardPinLeftArray = (VerticalPosToMotor);
+int * ForwardPinRightArray;
+ForwardPinRightArray = (VerticalPosToMotor);
+int * BackwardPinRightArray;
+BackwardPinLeftArray = (VerticalPosToMotor);
+  } while (DebugMod == true);
 }
